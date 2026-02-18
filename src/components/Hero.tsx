350d65f8061b0a1edb29d7242aae7ed0b01ed323
muyfresh <muyfreshbos@gmail.com>
2026-02-19 00:10:25 +0100

    Hero: adjust background to start higher and not cut off

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC = () => {
             {/* Stripe-style slanted background image */}
             <div className="absolute top-0 right-0 -z-10 w-full h-full pointer-events-none overflow-hidden">
                 <div
-                    className="absolute -top-[10%] -right-[15%] w-[100%] h-[120%] bg-no-repeat bg-right-top"
+                    className="absolute -top-[30%] -right-[10%] w-[110%] h-[150%] bg-no-repeat bg-right-top"
                     style={{
                         backgroundImage: 'url("/bg-hero.jpg")',
                         backgroundSize: 'contain',
